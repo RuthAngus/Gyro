bba7c5c0c742517cef3c07ff59209b267746e557
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2013-08-14 14:28:17 +0100

    B_V fixed

--- simple_minimise.py
+++ simple_minimise.py
@@ -18,48 +18,50 @@ p0 = [0.5189, 0.7725, 0.601]
 
 class Period_measurements(object):
 
-     def __init__(self, KIDs, measured_p, errors, B_Vs):
+     def __init__(self, KIDs, measured_p, errors, B_Vs, p0):
         self.KIDs = KIDs
         self.measured_p = measured_p
         self.errors = errors
         self.B_Vs = B_Vs
         self.p0 = p0
 
-    def minimise(self):
+     def minimise(self):
 
         nf = 0.56; af = 0.72; bf = 0.53 # Don't worry about these - just here for fake ages
-        model_ages = self.period_age(measured_p, nf, af, bf)        
+        model_ages = self.period_age(nf, af, bf)        
 
         ''' scipy.optimize.leastsq '''
 
         y = self.measured_p
         x = model_ages
-        plsq = leastsq(residuals, p0, args = (y, x))
+        plsq = leastsq(self.residuals, self.p0, args = (y, x))
         print plsq[0]
 
-    def residuals(self, p, y, x):
+     def residuals(self, p, y, x):
         # y = self.measured_p, x = model_ages
         #n, a, b = p
         #model_p = self.model_periods(p0)
-        err = y - model_periods(x, p[0], p[1], p[2])
+        err = y - self.model_periods(x)
         return err
 
-    def peval(self, x, p):
-        return model_periods(x, p[3], p[0], p[1], p[2])
+     def peval(self, x, p):
+        return self.model_periods(x)
 
-    def model_periods(self, x, n, a, b):
-        return age_period(x, n, a, b)
+     def model_periods(self, x):
+        return self.age_period(x)
 
-    def period_age(self, self.measured_p, n, a, b):
-        age = 10**((1./n)*(np.log10(self.measured_p)-np.log10(a)- b*(np.log10(self.B_Vs-0.4))))
+     def period_age(self, n, a, b):
+        age = 10**((1./n)*(np.log10(self.measured_p)-np.log10(a)-\
+                                    b*(np.log10(self.B_Vs-0.4))))
         x = self.B_Vs - 0.4
         errors = 0.02*(np.sqrt(3.0 + 0.5*np.log(age)**2.0+2.0*self.measured_p**0.6+\
                 (0.6/x)**2+(2.4*np.log(x))**2)) # FIXME
         return age
  
-    def age_period(self, age, n, a, b):
-        return 10**(n*(np.log10(age)) + np.log10(a) + b*(np.log10(self.B_Vs - 0.4)))
+     def age_period(self, age):
+        return 10**(self.p0[0]*(np.log10(age)) + np.log10(self.p0[1]) + \
+                    self.p0[2]*(np.log10(self.B_Vs - 0.4)))
 
    
-
-
+find_params = Period_measurements(KIDs, measured_p, errors, B_Vs, p0)
+find_params.minimise()
