9e67eabcb6c9800b1e65c78abbddd55f68841f81
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2013-08-16 14:08:37 +0100

    working

--- total_leastsq.py
+++ total_leastsq.py
@@ -17,7 +17,7 @@ measured_p = data[1]
 errors = data[2]
 B_Vs = data[3]
 B_Vs = 0.5 #FIXME
-p0 = [0.5189, 0.7725, 0.601]
+p0 = np.array([0.5189, 0.7725, 0.601])
 
 
 class totls2d(object):
@@ -29,26 +29,32 @@ class totls2d(object):
         self.y_err = y_err
         self.p0 = p0
 
-    def minimise(self):
-
+    def minimise(self, func, p0 = None):
+        if p0 == None:
+            p0 = self.p0
+        else:
+            self.p0 = p0            
         # Optimise parameters
-        new_p = fmin(gyro_cal.residuals, p0, args = (self.y, self.x))
+        new_p = fmin(func, p0)
+        final_resid = self.residuals(new_p)
+        # print 'MINIMISE...'
         print new_p
+        # print final_resid
+        # print '...MINIMISE'
+        self.params = new_p
 
         # Plotting
-        x_plot = np.arange(0, max(self.x), 10)
+        x_plot = np.r_[x.min():x.max():101j]
         B_Vs = 0.5 # FIXME        
-        y_plot = 10**(self.p0[0]*(np.log10(x_plot)) + np.log10(self.p0[1]) + \
-                                 self.p0[2]*(np.log10(B_Vs - 0.4)))
-        new_params_y_plot = 10**(new_p[0]*(np.log10(x_plot)) + np.log10(new_p[1]) + \
-                                 new_p[2]*(np.log10(B_Vs - 0.4)))
-        
+        y_plot = model_periods(p0, x_plot, B_Vs)
+        new_params_y_plot = model_periods(new_p, x_plot, B_Vs)
+
         p.close(1)
         p.figure(1)
         # p.subplot(2,1,1)
         p.plot(x_plot, y_plot, 'r-')
         p.plot(x_plot, new_params_y_plot, 'b-')
-        p.errorbar(self.x, self.y, yerr = self.y_err, fmt = 'k.')
+        p.errorbar(self.x, self.y, yerr = self.y_err, xerr = self.x_err, fmt = 'k.')
         # p.subplot(2,1,2)
         # p.plot(x_plot, y_plot, 'r-')
         # p.plot(x_plot, new_params_y_plot, 'b-')
@@ -57,33 +63,52 @@ class totls2d(object):
         p.xlabel('Age (Myr)')
         p.show()
     
-        def residuals(self, p, y, x):
-
-            # for each x, draw from gausssian and assign the same y value to each 
-            ndraws = 100
-            tot_x = np.zeros(ndraws*len(x))
-            tot_y = np.zeros(ndraws*len(x))
-            tot_yerr = np.zeros(ndraws*len(x))
-            n = 0
-            for i in range(len(x)):
-                for j in range(ndraws):
-                    tot_x[n] = random.gauss(x[i], x_err[i]) 
-                    tot_y[n] = y[i]
-                    tot_yerr[n] = yerr[i]
-                    n += 1
+    def residuals(self, params):
+        x, y, yerr = self.x, self.y, self.y_err
+        # for each x, draw from gausssian and assign the same y value to each 
+        ndraws = 100
+        tot_x = np.zeros(ndraws*len(x))
+        tot_y = np.zeros(ndraws*len(x))
+        tot_yerr = np.zeros(ndraws*len(x))
+        n = 0
+        for i in range(len(x)):
+            for j in range(ndraws):
+                tot_x[n] = random.gauss(x[i], x_err[i]) 
+                tot_y[n] = y[i]
+                tot_yerr[n] = yerr[i]
+                n += 1
                     
-            B_Vs = 0.5 # FIXME
-            model_periods = 10**(p[0]*(np.log10(tot_x)) + np.log10(p[1]) + \
-                                 p[2]*(np.log10(B_Vs - 0.4)))
-            err = sum((tot_y - model_periods(x))/tot_yerr)
-            return err
+        B_Vs = 0.5 # FIXME
+        err = sum(((tot_y - model_periods(params, tot_x, B_Vs))/tot_yerr)**2)
+        # print 'RESIDUALS...'
+        # print err
+        # #raw_input('Press return to continue')
+        # print '...RESIDUALS'        
+        return err
 
+def model_periods(params, x, B_Vs):
+    a = np.log10(x)
+    b = np.log10(B_Vs - 0.4)
+    c = params[0] * a + np.log10(params[1]) + params[2] * b
+    # print 'MODEL_PERIODS...'
+    # print params
+    # print a.min(), a.max()
+    # print b.min(), b.max()
+    # print c.min(), c.max()
+    # print '...MODEL_PERIODS'
+    return 10.0**c
 
 # Define x
 B_Vs = 0.5 # FIXME
 gyro_cal = gc.Period_measurements(KIDs, measured_p, errors, B_Vs, p0)
 x = gyro_cal.x_values()
-x_err = 0.35*np.ones(len(x)) # make up uncertainties
+x_err = 0.05*x # make up uncertainties
+p_true = np.array([0.56, 0.72, 0.53])
 
-find_params = totls2d(x, x_err, measured_p, errors, p0)
-find_params.minimise()
+F = totls2d(x, x_err, measured_p, errors, p0)
+# p.clf()
+# p.errorbar(F.x, F.y, xerr = F.x_err, yerr = F.y_err, fmt = 'ko')
+# p.plot(F.x, model_periods(p0, F.x, B_Vs), 'r+')
+# p.plot(F.x, model_periods(p_true, F.x, B_Vs), 'cx')
+#print F.residuals(p0)
+F.minimise(F.residuals)
