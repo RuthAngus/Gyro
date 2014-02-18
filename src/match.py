e0dcc89105c887b037bd5540ac1c13e198dcde34
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2014-02-18 15:44:20 +0000

    adding assemble function

--- src/match.py
+++ src/match.py
@@ -16,12 +16,39 @@ def match(KID, X):
             Xm[:,i] = X[:,l].reshape(Xm[:,i].shape)
     return Xm
 
-# # Assemble data from various places and concatenate
-# data = np.genfromtxt('/Users/angusr/Python/Gyro/all_data.txt').T
-# l = data[0] != 10124866.0 # Remove missing data point (now only have 42)
-# KID = data[0,l]
-# period = data[1,l]
-# period_err = data[2,l]
-#
-# # Load Astero data
-# data = np.genfromtxt('/Users/angusr/Desktop/astero_ages.txt').T
+def assemble():
+
+        # load period data
+    pdata = np.genfromtxt('/Users/angusr/Python/Gyro/data/all_data.txt').T
+    KID = pdata[0]
+    p = pdata[1]
+    p_err = pdata[2]
+
+        # Load Astero data from table 1 - KIDs, teffs and feh
+        # Columns: KID, nu, nu_err, dnu, dnu_err, SDSS_teff, st_err,
+        # IRFM_teff, It_err, feh, feh_err
+    data1 = np.genfromtxt('/Users/angusr/Python/Gyro/data/ApJS91604R2tables.txt', \
+            skiprows=30, skip_footer=1343, invalid_raise=False, usecols=(0,5,6,9,10)).T
+    table1 = match(KID, data1)
+
+        # load astero data from table 5 - KID, mass, logg, age
+    data2 = np.genfromtxt('/Users/angusr/Python/Gyro/data/ApJS91604R2tables.txt', \
+            skiprows=699, skip_footer=675, invalid_raise=False, \
+            usecols=(0,1,2,3,11,12,13,14,15)).T
+    table5 = match(KID, data2)
+
+        # Assemble data in the following order:
+        # KID, period, p_err, teff, teff_err, feh, feh_err, mass, mass_errp,
+        # mass_errm, logg, logg_errp, logg_errm, age, age_errp, age_errm
+    data = np.ndarray((len(KID), 16))
+    data[:,0] = KID
+    data[:,1] = p
+    data[:,2] = p_err
+    data[:,3:7] = table1[1:,:].T
+    data[:,8:16] = table5[1:,:].T
+
+    return data
+
+data = assemble()
+# pickle.dump(data, '/Users/angusr/Python/Gyro/data/data.txt'))
+np.savetxt("/Users/angusr/Python/Gyro/data/data.txt", data)
