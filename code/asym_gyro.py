e5e5f5a07b6273d552c1fd70154dd83a40409121
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2014-10-27 17:36:55 -0400

    adding sampling function

--- code/asym_gyro.py
+++ code/asym_gyro.py
@@ -6,6 +6,7 @@ from all_plotting import load_dat
 import h5py
 import datetime
 from gyro_like import lnlike, period_model
+from sampling import asym_sample
 
 def lnprior(m):
     if -10. < m[0] < 10. and .0< m[1] < 1. and 0. < m[2] < 1. \
@@ -50,45 +51,9 @@ def MCMC(fname, c):
 #     pl.show()
 
     # Now generate samples
-    nsamp, nrough = 100, 1000
-    s = 12
-    age_samp = np.zeros((len(age_obs), nsamp))
-    logg_samp = np.zeros((len(age_obs), nsamp))
-    for i, obs in enumerate(age_obs):
-        np.random.seed(s)
-        age_sampp = obs+age_errp[i]*np.random.randn(nrough)
-        np.random.seed(s)
-        age_sampm = obs+age_errm[i]*np.random.randn(nrough)
-        lp, lm = age_sampp>obs, age_sampm<obs
-        age_samp_temp = np.concatenate((age_sampp[lp], age_sampm[lm]))
-        r = np.random.randint(0, nrough, nsamp)
-        age_samp[i, :] = age_samp_temp[r]
-
-        np.random.seed(s)
-        logg_sampp = logg_obs[i]+logg_errp[i]*np.random.randn(nrough)
-        np.random.seed(s)
-        logg_sampm = logg_obs[i]+logg_errm[i]*np.random.randn(nrough)
-        lp, lm = logg_sampp>logg_obs[i], logg_sampm<logg_obs[i]
-        logg_samp_temp = np.concatenate((logg_sampp[lp], logg_sampm[lm]))
-        r = np.random.randint(0, nrough, nsamp)
-        logg_samp[i, :] = logg_samp_temp[r]
-
-#         pl.clf()
-#         pl.subplot(2,1,1)
-#         pl.hist(age_sampp, 50, color='r', alpha=.3)
-#         pl.hist(age_sampm, 50, color='b', alpha=.3)
-#         pl.subplot(2,1,2)
-#         pl.hist(age_sampp[lp], 50, color='r', alpha=.3)
-#         pl.hist(age_sampm[lm], 50, color='b', alpha=.3)
-#         pl.hist(age_samp, 100, color='k', alpha=.3)
-#         pl.xlim(-2, 14)
-#         pl.show()
-#         raw_input('etner')
-
-    np.random.seed(s)
-    bv_samp = np.vstack([x0+xe*np.random.randn(nsamp) for x0, xe in zip(bv_obs, bv_err)])
-    np.random.seed(s)
-    period_samp = np.vstack([x0+xe*np.random.randn(nsamp) for x0, xe in zip(period_obs, period_err)])
+    age_samp, bv_samp, logg_samp, period_samp = \
+            asym_sample(age_obs, age_err, bv_obs, bv_err, period_obs,
+                        period_err, nsamp, s)
 
     print 'initial likelihood = ', lnlike(par_true, age_samp, bv_samp, \
             period_samp, logg_samp, age_obs, age_err, bv_obs, bv_err, period_obs, period_err, \
