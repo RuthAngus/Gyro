f881c57ed7f0d6ca551783bcbb279f71fbd85b5a
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2014-11-01 12:39:46 -0400

    properly normalised

--- code/sampling.py
+++ code/sampling.py
@@ -20,7 +20,8 @@ def simple_sample(age_obs, age_err, bv_obs, bv_err, period_obs, period_err,
 def asym_sample(age_obs, age_errp, age_errm, bv_obs, bv_err, period_obs, period_err,
                 logg_obs, logg_errp, logg_errm, nsamp, s):
 
-    nrough = 1000
+    nrough = 100000
+    nsamp = 50000
     age_samp = np.zeros((len(age_obs), nsamp))
     logg_samp = np.zeros((len(age_obs), nsamp))
     for i, obs in enumerate(age_obs):
@@ -29,42 +30,46 @@ def asym_sample(age_obs, age_errp, age_errm, bv_obs, bv_err, period_obs, period_
         age_sampp = obs+age_errp[i]*np.random.randn(nrough)
         np.random.seed(s)
         age_sampm = obs+age_errm[i]*np.random.randn(nrough)
-        lp, lm = age_sampp>obs, age_sampm<obs
-        age_samp_temp = np.concatenate((age_sampp[lp], age_sampm[lm]))
-        r = np.random.randint(0, nrough, nsamp)
-        age_samp[i, :] = age_samp_temp[r]
+        varp, varm = np.std(age_sampp), np.std(age_sampm)
+        r = varm/varp
+        age_samp_tempp = obs+age_errp[i]*np.random.randn(nrough)
+        age_samp_tempm = obs+age_errm[i]*np.random.randn(nrough*r)
+        lp, lm = age_samp_tempp > obs, age_samp_tempm < obs
+        age_samp_temp = np.concatenate((age_samp_tempp[lp], age_samp_tempm[lm]))
+        ra = np.random.randint(0, len(age_samp_temp), nsamp)
+        age_samp[i, :] = age_samp_temp[ra]
 
         np.random.seed(s)
         logg_sampp = logg_obs[i]+logg_errp[i]*np.random.randn(nrough)
         np.random.seed(s)
         logg_sampm = logg_obs[i]+logg_errm[i]*np.random.randn(nrough)
-        lp, lm = logg_sampp>logg_obs[i], logg_sampm<logg_obs[i]
-        logg_samp_temp = np.concatenate((logg_sampp[lp], logg_sampm[lm]))
-        r = np.random.randint(0, nrough, nsamp)
-        logg_samp[i, :] = logg_samp_temp[r]
+        varp, varm = np.std(logg_sampp), np.std(logg_sampm)
+        r = varm/varp
+        logg_samp_tempp = obs+logg_errp[i]*np.random.randn(nrough)
+        logg_samp_tempm = obs+logg_errm[i]*np.random.randn(nrough*r)
+        lp, lm = logg_samp_tempp > obs, logg_samp_tempm < obs
+        logg_samp_temp = np.concatenate((logg_samp_tempp[lp], logg_samp_tempm[lm]))
+        ra = np.random.randint(0, len(logg_samp_temp), nsamp)
+        logg_samp[i, :] = logg_samp_temp[ra]
+        print logg_errp[i], logg_errm[i]
 
 #         pl.clf()
 #         pl.subplot(2,1,1)
-#         pl.hist(age_sampp, 50, color='r', alpha=.3)
-#         pl.hist(age_sampm, 50, color='b', alpha=.3)
+#         pl.hist(age_sampp, 50, normed=True, color='r', alpha=.3)
+#         pl.hist(age_sampm, 50, normed=True, color='b', alpha=.3)
 #         pl.subplot(2,1,2)
-#         pl.hist(age_sampp[lp], 50, color='r', alpha=.3)
-#         pl.hist(age_sampm[lm], 50, color='b', alpha=.3)
-#         pl.hist(age_samp[i, :], 100, alpha=.3)
+#         pl.hist(age_samp[i, :], 100, normed=True, alpha=.3)
 #         pl.xlim(-2, 14)
 #         pl.show()
-#         raw_input('etner')
 #
 #         pl.clf()
 #         pl.subplot(2,1,1)
-#         pl.hist(logg_sampp, 50, color='r', alpha=.3)
-#         pl.hist(logg_sampm, 50, color='b', alpha=.3)
+#         pl.hist(logg_sampp, 50, normed=True, color='r', alpha=.3)
+#         pl.hist(logg_sampm, 50, normed=True, color='b', alpha=.3)
 #         pl.subplot(2,1,2)
-#         pl.hist(logg_sampp[lp], 50, color='r', alpha=.3)
-#         pl.hist(logg_sampm[lm], 50, color='b', alpha=.3)
-#         pl.hist(logg_samp[i, :], 100, alpha=.3)
+#         pl.hist(logg_samp[i, :], 100, normed=True, alpha=.3)
 #         pl.show()
-#         raw_input('etner')
+#         raw_input('enter')
 
     np.random.seed(s)
     bv_samp = np.vstack([x0+xe*np.random.randn(nsamp) for x0, xe in zip(bv_obs, bv_err)])
